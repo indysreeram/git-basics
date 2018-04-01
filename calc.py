952db961bc738b842d6859bdfc4c6f50b5292a8d
Sreeram Srinivasaragavan <indysreeram@gmail.com>
2018-04-01 10:47:40 -0400

    added addition and multiply

--- calc.py
+++ calc.py
@@ -1,7 +1,13 @@
-print("I am a clac python mania")
+print("I am a calc python mania")
 
 def divide(x,y):
     return x/y
 
 def subtract(x,y):
     return x-y
+
+def add(x,y):
+    return x+y
+
+def multiply(x,y):
+    return x*y
